24457065cd430f3e5da778eef436a858a57c5350
SutanMandava <50817297+SutanMandava@users.noreply.github.com>
2024-03-22 01:14:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById('myHeading').innerHTML = 'I am Sutan Mandava!';
+document.getElementById('myParagraph').innerHTML = 'I am from Vellore Institute of Technology, Registration Number: 22BCT0350';
+
+document.getElementById('myHeading').style.color = '#e74c3c';
+document.getElementById('myParagraph').style.color = '#f39c12';
+document.getElementById('myImage').style.border = '2px solid #3498db';
+
+document.getElementById('myImage').src = 'new-image-url.jpg';
+document.getElementById('myImage').alt = 'New Image Alt Text';
